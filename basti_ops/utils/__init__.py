a3a74cc6758fc12a421e78b8371be5bbb978281a
Bastian Pastoors <bastian.pastoors@wooga.net>
2025-10-10 07:59:54 +0200

    chore: cleanup

--- basti_ops/utils/__init__.py
+++ basti_ops/utils/__init__.py
@@ -1,23 +1,5 @@
-# import importlib
-
 from . import material
 from . import mesh
 from . import object
 from . import raycast
 from . import selection
-
-# modules = [
-#     util_material,
-#     util_mesh,
-#     util_object,
-#     util_raycast,
-#     util_selection,
-# ]
-#
-# def register():
-#     for m in modules:
-#         importlib.reload(m)
-#
-#
-# def unregister():
-#     pass
