b633c7d7610dc708a692bc1bcba58fb129ed82e0
Bastian Pastoors <bastian.pastoors@wooga.net>
2025-10-10 08:01:07 +0200

    fix: material assigning and creation

--- basti_ops/utils/material.py
+++ basti_ops/utils/material.py
@@ -35,40 +35,29 @@ def create_new_material():
         if bpy.data.materials.find(find_name) == -1:
             material_found = False
             new_name = find_name
-    return bpy.data.materials.new(new_name)
+    material = bpy.data.materials.new(new_name)
+    material.use_nodes = True
+    return material
 
 
 def apply_material_on_selected_faces(context, material):
-    obj_active = context.active_object
-
     objs_selected = [obj for obj in context.selected_objects if obj.type == "MESH"]
+    context_mode = "OBJECT" if context.mode == "OBJECT" else "EDIT"
+    bpy.ops.object.mode_set(mode="OBJECT")
 
     for obj in objs_selected:
-        bpy.context.view_layer.objects.active = obj
-        if context.mode == "OBJECT":
-            polys_selected = obj.data.polygons
-            stored_mode = "OBJECT"
-        else:
-            stored_mode = "EDIT"
-
-        bpy.ops.object.mode_set(mode="OBJECT")
-        polys_selected = get_all_selected_polygons(obj, True)
+        polys_selected = obj.data.polygons if context_mode == "OBJECT" else get_all_selected_polygons(obj, True)
 
         if len(obj.material_slots) == 0:
-            new_mat_index = -1
-        else:
-            material_list = [slot.material for slot in obj.material_slots]
-            try:
-                new_mat_index = material_list.index(material)
-            except ValueError:
-                new_mat_index = -1
+            obj.data.materials.append(material)
 
-        if new_mat_index == -1:
-            obj.data.materials.append(new_mat)
-            new_mat_index = len(obj.data.materials)
+        material_list = [slot.material for slot in obj.material_slots]
+        if material in material_list:
+            new_mat_index = material_list.index(material)
+        else:
+            obj.data.materials.append(material)
+            new_mat_index = len(material_list)
 
         for poly in polys_selected:
             poly.material_index = new_mat_index
-        bpy.ops.object.mode_set(mode=stored_mode)
-
-    bpy.context.view_layer.objects.active = obj_active
+    bpy.ops.object.mode_set(mode=context_mode)
