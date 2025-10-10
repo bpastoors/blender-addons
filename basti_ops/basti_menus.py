6c188f665eca2e910369bbf573fd0d9ac6279a50
Bastian Pastoors <bastian.pastoors@wooga.net>
2025-10-10 11:56:25 +0200

    fix: rearranging order of pie menu

--- basti_ops/basti_menus.py
+++ basti_ops/basti_menus.py
@@ -4,20 +4,28 @@ class VIEW3D_MT_BastiCreateAndCenter(bpy.types.Menu):
     bl_label = "Create and Center"
 
     def draw(self, context):
+        object_mode = context.mode == "OBJECT"
         layout = self.layout
         pie = layout.menu_pie()
 
-        pie.operator("mesh.primitive_plane_add")
-        pie.operator("mesh.primitive_cube_add")
-        pie.operator("mesh.primitive_cylinder_add")
+        #left
+        pie.operator("mesh.primitive_plane_add", text="Draw Polygon")
+        #right
         pie.operator("mesh.primitive_uv_sphere_add")
-        op = pie.operator("wm.call_menu", text="Scale to zero")
-        op.name = "VIEW3D_MT_BastiScaleToZero"
+        #down
         op = pie.operator("wm.call_menu", text="Move to zero")
         op.name = "VIEW3D_MT_BastiMoveToZero"
+        #up
+        pie.operator("mesh.primitive_cube_add")
+        #up-left
+        pie.operator("mesh.primitive_plane_add")
+        #up-right
+        pie.operator("mesh.primitive_cylinder_add")
+        #down-left
+        op = pie.operator("wm.call_menu", text="Scale to zero")
+        op.name = "VIEW3D_MT_BastiScaleToZero"
+        #down-right
         op = pie.operator("wm.call_menu_pie", text="Duplicate")
-        op.name = "VIEW3D_MT_BastiDuplicate"
-        op = pie.operator("wm.call_menu_pie", text="Duplicate2")
         op.name = "VIEW3D_MT_BastiDuplicate"
 
 class VIEW3D_MT_BastiDuplicate(bpy.types.Menu):
