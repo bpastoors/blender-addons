28c221b62dabdea485245447361645fdf2c1c664
Bastian Pastoors <bastian.pastoors@wooga.net>
2025-11-18 10:22:52 +0100

    version bump

--- basti_ops/__init__.py
+++ basti_ops/__init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "author": "Bastian",
     "description": "",
     "blender": (4, 5, 0),
-    "version": (0, 1, 0),
+    "version": (0, 2, 0),
     "location": "",
     "warning": "",
     "category": "Generic",
