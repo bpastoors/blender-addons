a8bd96a2ea3b431e04c950e87ceefae014ceda92
Bastian Pastoors <bastian.pastoors@wooga.net>
2025-12-17 08:02:19 +0100

    feat: adding automatic population of overview panel

--- basti_ops/panels/overview.py
+++ basti_ops/panels/overview.py
@@ -1,81 +1,57 @@
 import bpy
+from pathlib import Path
 
 
 class VIEW3D_PT_BastiOverview(bpy.types.Panel):
-    bl_label = "Cursor Properties"
+    bl_label = "Overview"
     bl_idname = "VIEW3D_PT_basti_overview"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "Basti Ops"
+
+    @staticmethod
+    def get_content(target: str, pie_menu: bool = False):
+        path_operators = Path(__file__).parent.parent / target
+        if target == "operators":
+            for file in path_operators.glob("*.py"):
+                yield file.stem
+        for file in path_operators.glob("*.py"):
+            file_text = file.read_text()
+            label = file_text.split('bl_label = "', 1)[1].split('"', 1)[0]
+            if target == "menus":
+                pie_in_text = "pie = layout.menu_pie()" in file_text
+                if pie_menu != pie_in_text:
+                    continue
+                class_name = file_text.split("class ", 1)[1].split("(bpy.", 1)[0]
+                yield class_name, label
+            if target == "panels":
+                if label == "Overview":
+                    continue
+                id_name = file_text.split('bl_idname = "', 1)[1].split('"', 1)[0]
+                yield id_name, label
 
     def draw(self, context):
         layout = self.layout
 
         layout.separator(type="LINE")
         layout.label(text="Menus")
-        for menu in [
-            ("VIEW3D_MT_BastiQuickMirror", "Quick Mirror"),
-            ("VIEW3D_MT_BastiScaleToZero", "Scale to Zero"),
-            ("VIEW3D_MT_BastiMoveToZero", "Move to Zero"),
-        ]:
+        for menu in self.get_content("menus", False):
             op = layout.operator("wm.call_menu", text=menu[1])
             op.name = menu[0]
 
         layout.separator(type="LINE")
         layout.label(text="Pie-Menus")
-        for menu in [
-            ("VIEW3D_MT_Basti3dSettings", "3d Settings"),
-            ("VIEW3D_MT_Basti3dView", "3d View"),
-            ("VIEW3D_MT_BastiModeling", "Modeling"),
-            ("VIEW3D_MT_BastiCreateAndCenter", "Create and Center"),
-            ("VIEW3D_MT_BastiDuplicate", "Duplicate"),
-        ]:
-            op = layout.operator("wm.call_menu_pie", text=menu[1])
+        for menu in self.get_content("menus", True):
+            op = layout.operator("wm.call_menu", text=menu[1])
             op.name = menu[0]
 
         layout.separator(type="LINE")
         layout.label(text="Panels")
-        for panel in [
-            ("VIEW3D_PT_basti_action_center", "Action Center"),
-            ("VIEW3D_PT_basti_cursor", "Cursor"),
-        ]:
+        for panel in self.get_content("panels"):
             op = layout.operator("wm.call_panel", text=panel[1])
             op.name = panel[0]
 
         layout.separator(type="LINE")
         layout.label(text="Operators")
-        for op in [
-            "bevel",
-            "connect_or_knife",
-            "copy_to_clipboard",
-            "delete",
-            "linear_array",
-            "loop_slice",
-            "make_face",
-            "make_polygon",
-            "merge_by_type",
-            "move_to_zero",
-            "paste_from_clipboard",
-            "quick_mirror",
-            "radial_array",
-            "rotate_to_zero",
-            "scale_to_zero",
-            "scatter_duplicate",
-            "select_edge_or_island",
-            "select_loop",
-            "set_action_center",
-            "set_cursor",
-            "set_selection_mode",
-            "set_viewpoint",
-            "toggle_sculpt_automasking",
-        ]:
-            layout.operator(f"basti.{op}")
-
-        layout.separator(type="LINE")
-        layout.label(text="Operators with Mouse-Target")
-        for op in [
-            "apply_material",
-            "copy_to_mesh",
-            "move_to_face",
-        ]:
+        for op in self.get_content("operators"):
             layout.operator(f"basti.{op}")
