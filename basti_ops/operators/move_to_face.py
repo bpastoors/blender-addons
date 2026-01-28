5b770a2971c42a1bfc74d9f1957421478cbb206e
Bastian Pastoors <bastian.pastoors@wooga.net>
2026-01-28 08:54:33 +0100

    chore: updating and cleaning up

--- basti_ops/operators/move_to_face.py
+++ basti_ops/operators/move_to_face.py
@@ -7,9 +7,11 @@ from mathutils import Vector, Quaternion
 
 from ..utils.selection import (
     get_linked_verts,
-    get_selected_bm_vertices,
     get_mesh_selection_mode,
     get_selected_polygons,
+    get_selected_vertices,
+    select_by_id,
+    get_selected,
 )
 from ..utils.mesh import get_average_location, get_average_normal, rotate_vertices
 from ..utils.raycast import raycast
@@ -46,19 +48,23 @@ class BastiMoveToFace(bpy.types.Operator):
         self,
         objs: list[bpy.types.Mesh],
         location: Vector,
+        selection_mode: str,
         normal: Optional[Vector] = None,
     ):
         """Move submeshes to the point and rotate them to the normal"""
         obj_data = []
         average_location = Vector((0.0, 0.0, 0.0))
-        selection_mode = get_mesh_selection_mode(bpy.context)
 
         for obj in objs:
+            selection_ids = get_selected(obj, selection_mode, get_index=True)
+            selected_vert_ids = get_selected_vertices(obj, get_index=True)
+            linked_vert_ids = get_linked_verts(obj, get_index=True)
             bm = bmesh.from_edit_mesh(obj.data)
-            bm_verts_selected = get_selected_bm_vertices(bm, obj)
-            bm_verts_island = get_linked_verts(obj, bm, bm_verts_selected)
+            bm_verts_island = [bm.verts[v_id] for v_id in linked_vert_ids]
 
-            average_location_obj = get_average_location(bm_verts_selected, obj)
+            average_location_obj = get_average_location(
+                [bm.verts[v_id] for v_id in selected_vert_ids], obj
+            )
             if normal and selection_mode == "FACE":
                 average_normal = get_average_normal(get_selected_polygons(obj), obj)
                 rotation_difference = average_normal.rotation_difference(normal * -1)
@@ -71,7 +77,8 @@ class BastiMoveToFace(bpy.types.Operator):
             obj_data.append(
                 {
                     "object": obj,
-                    "selection_indexes": [v.index for v in bm_verts_island],
+                    "island_ids": linked_vert_ids,
+                    "selection_ids": selection_ids,
                 }
             )
             bm.free()
@@ -83,7 +90,7 @@ class BastiMoveToFace(bpy.types.Operator):
             obj = obj_data_entry["object"]
             bm = bmesh.from_edit_mesh(obj.data)
             bm.verts.ensure_lookup_table()
-            verts = [bm.verts[i] for i in obj_data_entry["selection_indexes"]]
+            verts = [bm.verts[i] for i in obj_data_entry["island_ids"]]
             for vert in verts:
                 vert_location = obj.matrix_world @ vert.co.copy()
                 vert_location -= move_offset
@@ -95,6 +102,7 @@ class BastiMoveToFace(bpy.types.Operator):
             bm.normal_update()
             bmesh.update_edit_mesh(obj.data)
             bm.free()
+            select_by_id(obj, selection_mode, obj_data_entry["selection_ids"])
 
     def move_objects_to_point(
         self,
@@ -125,18 +133,20 @@ class BastiMoveToFace(bpy.types.Operator):
         raycast_result, location, normal, _, obj_target = raycast(context, coords)
         if not raycast_result:
             return
+        selection_mode = get_mesh_selection_mode(context)
         obj_active = context.active_object
-
         objs_selected = [obj for obj in context.selected_objects if obj.type == "MESH"]
 
         if not self.orient:
             normal = None
 
-        if context.mode == "OBJECT":
+        if selection_mode == "OBJECT":
             self.move_objects_to_point(objs_selected, Vector(location), normal)
         else:
             if len(objs_selected) == 0:
                 objs_selected = [obj_active]
-            self.move_submeshes_to_point(objs_selected, Vector(location), normal)
+            self.move_submeshes_to_point(
+                objs_selected, Vector(location), selection_mode, normal
+            )
 
         bpy.context.view_layer.objects.active = obj_active
